0bf3612700c55f75715bb48af0c5d44bec49e1dd
macosecheverria <marcosdarkecheverria12@gmail.com>
2022-10-28 16:05:45 -0300

    ejercicio variables open bootcamp

---
+++ ejercicio2.js
@@ -0,0 +1,17 @@
+//ejercicio 2
+
+
+
+const listasDatos = ["Marcos Echeverria",23,true];
+console.log(listasDatos)
+
+const nacimiento = new Date(1998, 10,10);
+console.log(nacimiento);
+
+
+const libro={
+    titulo: "Blade Runner",
+    autor: "Philip K. Dick",
+    fecha: 1968,
+    url: "https://www.academia.edu/34755000/Blade_Runner_Suenan_los_andro_Philip_K_Dick"
+}
